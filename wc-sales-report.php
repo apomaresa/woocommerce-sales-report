9ce24a90fe5974568271bf26d4df1006c1772a6e
Abel Pomares <a.pomares.a@hotmail.com>
2024-09-23 22:28:48 -0500

    Add plugin header and security checks to prevent direct access

---
+++ wc-sales-report.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: WooCommerce Sales Report
+ * Description: Plugin that displays an international sales report on the WordPress dashboard.
+ * Version: 1.0
+ * Author: Abel Pomares A
+ * License: GPLv3 or later
+ */
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit; // Terminate if accessed outside of WordPress
+}
