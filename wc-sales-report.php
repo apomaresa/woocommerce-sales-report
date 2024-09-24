ca933f8377ead6a1f03155cd0c3aaddaf7621088
Abel Pomares <a.pomares.a@hotmail.com>
2024-09-23 22:31:44 -0500

    Add function to create custom table for storing sales data by country

--- wc-sales-report.php
+++ wc-sales-report.php
@@ -12,3 +12,24 @@
 if (!defined('ABSPATH')) {
     exit; // Terminate if accessed outside of WordPress
 }
+
+// Create custom table to store sales by country when the plugin is activated
+register_activation_hook(__FILE__, 'wc_sales_report_create_table');
+
+function wc_sales_report_create_table()
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'sales_by_country';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id mediumint(9) NOT NULL AUTO_INCREMENT,
+        country varchar(100) NOT NULL,
+        total_sales decimal(10,2) NOT NULL,
+        date datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
+        PRIMARY KEY (id)
+    ) $charset_collate;";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+}
